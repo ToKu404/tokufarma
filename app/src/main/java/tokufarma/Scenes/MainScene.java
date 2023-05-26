ddf6fcc80ae48e16b1bfbfaf9023dd0c10e1bbe0
MrPipsterr <farrelpalinggi@gmail.com>
2023-05-26 14:48:40 +0800

    feat: add dan remove apoteker

--- app/src/main/java/tokufarma/Scenes/MainScene.java
+++ app/src/main/java/tokufarma/Scenes/MainScene.java
@@ -6,8 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -51,8 +53,24 @@ public class MainScene {
         //pasangkan
         listViewPharmas.setItems(listPharmas);
 
+        //input
+        TextField tfName = new TextField();
+        Button btnAdd = new Button("Tambah");
+        Button btnRemove = new Button("Hapus");
+
+        //tambah apoteker
+        btnAdd.setOnAction(v -> {
+            listPharmas.add(tfName.getText());
+        });
+
+        //hapus apoteker
+        btnRemove.setOnAction(v -> {
+            int index = listViewPharmas.getSelectionModel().getSelectedIndex();
+            listPharmas.remove(index);
+        });
+
         //tambah listview ke vbox
-        rightSide.getChildren().add(listViewPharmas);
+        rightSide.getChildren().addAll(listViewPharmas, tfName, btnAdd, btnRemove);
     }
 
     private void showTableView() {
