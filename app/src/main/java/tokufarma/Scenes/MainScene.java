e78905103953b08201f346c1baa96db512daf779
fadillahcheryl <fadillahcheryl88@gmail.com>
2023-05-26 14:33:37 +0800

    feat: membuat list view

--- app/src/main/java/tokufarma/Scenes/MainScene.java
+++ app/src/main/java/tokufarma/Scenes/MainScene.java
@@ -1,5 +1,8 @@
 package tokufarma.Scenes;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -37,6 +40,18 @@ public class MainScene {
 
     private void showListView() {
         rightSide.getChildren().clear();
+        //Observable List
+        ObservableList<String> listPharmas = FXCollections.observableArrayList();
+        listPharmas.addAll("Afifah", "Salsabila", "Seril", "Freki");
+
+
+        //menampilkan listview apoteker
+        ListView<String> listViewPharmas = new ListView<>();
+        //pasangkan
+        listViewPharmas.setItems(listPharmas);
+
+        //tambahkan listview ke vbox
+        rightSide.getChildren().add(listViewPharmas);
     }
 
     private void showTableView() {
