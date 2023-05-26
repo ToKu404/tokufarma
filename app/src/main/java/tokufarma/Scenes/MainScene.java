519b928ad4c87d18cc316cadc244a0ddba75a709
= <=>
2023-05-26 14:31:26 +0800

    feat : Menambahkan list view

--- app/src/main/java/tokufarma/Scenes/MainScene.java
+++ app/src/main/java/tokufarma/Scenes/MainScene.java
@@ -1,5 +1,9 @@
 package tokufarma.Scenes;
 
+import java.util.Observable;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -37,6 +41,17 @@ public class MainScene {
 
     private void showListView() {
         rightSide.getChildren().clear();
+
+        //Observable list
+        ObservableList<String> listPharmas = FXCollections.observableArrayList();
+
+        //Menambahkan Elemen nama apoteker
+        listPharmas.addAll("Apt.Ikhsan Malik", "Apt.Ridwan Kamil", "Apt.Syahrul Munir", "Apt.Lowrence Brick");
+
+        ListView<String> listViewPharmas = new ListView<>(null);
+        listViewPharmas.setItems(listPharmas);
+
+        rightSide.getChildren().add(listViewPharmas);
     }
 
     private void showTableView() {
