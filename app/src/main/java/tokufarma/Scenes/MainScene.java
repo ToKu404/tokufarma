b735d070ccf680a64f1ca1d4fe69fcbae62feae9
Susantii92 <susantii9261@gmail.com>
2023-05-26 14:35:47 +0800

    feat: membuat List View

--- app/src/main/java/tokufarma/Scenes/MainScene.java
+++ app/src/main/java/tokufarma/Scenes/MainScene.java
@@ -1,5 +1,7 @@
 package tokufarma.Scenes;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -37,7 +39,20 @@ public class MainScene {
 
     private void showListView() {
         rightSide.getChildren().clear();
+        //observable list
+        ObservableList<String> listPharmas = FXCollections.observableArrayList();
+        listPharmas.addAll("santi, muthia");
+
+        //menampilkan list apoteker
+        ListView listViewPharmas = new ListView<>();
+
+        //pasangkan
+        listViewPharmas.setItems(listPharmas);
+
+        //tambah listview ke VBox
+        rightSide.getChildren().add(listViewPharmas);
     }
+
 
     private void showTableView() {
         rightSide.getChildren().clear();
