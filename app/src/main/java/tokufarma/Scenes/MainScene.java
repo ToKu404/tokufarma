198b8e73f917ac93b8a869b89e54b149560ec0c2
Rasyad Bimasatya <rasyadbimasatya04@gmail.com>
2023-05-26 14:46:38 +0800

    feat: menambah button tambah dan remove

--- app/src/main/java/tokufarma/Scenes/MainScene.java
+++ app/src/main/java/tokufarma/Scenes/MainScene.java
@@ -4,8 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -49,7 +51,21 @@ public class MainScene {
         //pasangkan
         listviewPharmas.setItems(listPharmas);
         //tambah listview ke vbox
-        rightSide.getChildren().add(listviewPharmas);
+
+        TextField tfName = new TextField();
+        Button btnAdd = new Button("Tambah");
+        Button btnRemove = new Button("Delete");
+
+        btnAdd.setOnAction(v -> {
+            listPharmas.add(tfName.getText());
+        });
+
+        btnRemove.setOnAction(v -> {
+            int index = listviewPharmas.getSelectionModel().getSelectedIndex();
+            listPharmas.remove(index);
+        });
+
+        rightSide.getChildren().addAll(listviewPharmas, tfName, btnAdd, btnRemove);
     }
 
     private void showTableView() {
